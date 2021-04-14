23ce4ec8d243430f62b3798f0fd37b8f9993617f
Huailong-cheng <2893773348@qq.com>
2021-04-14 15:15:40 +0800

    静态页面部分大部分已完成

--- graduationProgram/pages/index/index.vue
+++ graduationProgram/pages/index/index.vue
@@ -1,52 +1,85 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+		<u-search placeholder="日照香炉生紫烟" v-model="keyword" :clearabled="true" @search="search()" :show-action="false"></u-search>
+		<!-- //底部三个按钮 -->
+		<view class="areaChoose">
+			<view>五山校区</view>
+			<view>大学城校区</view>
+			<view>国际校区</view>
 		</view>
+		
+		
+		
+		
+		<u-tabbar v-model="current" :list="list" :mid-button="true"></u-tabbar>
+		
 	</view>
+	
+	
+	
 </template>
 
 <script>
-	export default {
+	export default {		
 		data() {
 			return {
-				title: 'Hello'
+				keyword: 'Hello',
+				list: [{
+						iconPath: "home",
+						selectedIconPath: "home-fill",
+						text: '寻找故事',												
+						customIcon: false,
+					},				
+					{
+						iconPath: "https://cdn.uviewui.com/uview/common/min_button.png",
+						selectedIconPath: "https://cdn.uviewui.com/uview/common/min_button_select.png",
+						text: '发布故事',
+						midButton: true,
+						customIcon: false,
+					},					
+					{
+						iconPath: "account",
+						selectedIconPath: "account-fill",
+						text: '个人中心',
+						isDot: false,
+						customIcon: false,
+					},
+				],
+				current: 0
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
+			search(){
+				uni.navigateTo({
+					url:'../searchPlace/searchPlace'
+				})
+			}
 
-		}
+		},
+		// onReady(){
+		// 	search(){
+				
+		// 	}
+		// }
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	.areaChoose{
+		display:flex;
+		flex-direction: row;
+		justify-content: space-around;
+		margin-top:50rpx
+		
+	} 
+	.areaChoose view{
+		border:#007AFF 3rpx solid;
+		padding:10rpx;
+		border-radius: 10rpx;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
+	
 </style>
---
+++ graduationProgram/pages/search/search.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<u-search placeholder="日照香炉生紫烟" v-model="keyword"></u-search>
+		<view class="history">历史记录</view>
+		<view class="historyData" v-for="message in historyData" >地点：{{message}}</view>
+		<view class="history">搜索发现</view>
+		<view class="historyData" v-for="message in historyData" >地点：{{message}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				keyword: '遥看瀑布挂前川',
+				historyData:['故事文字第一句','故事文字第二句','故事文字第三句','故事文字第四句']
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+		
+	.history{
+		margin-top: 10rpx;
+	}
+	.historyData{
+		padding:10rpx 100rpx;
+		margin-left:30rpx;
+		margin-right:30rpx;
+		margin-top: 10rpx;
+		border:solid #007AFF 3rpx
+	}
+	
+	
+
+</style>
