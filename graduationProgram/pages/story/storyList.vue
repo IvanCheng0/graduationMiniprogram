97c4f01af3caa6a8b9f534a253da7768b6dd337a
little-ball <49150035+LitBall@users.noreply.github.com>
2021-06-20 22:50:44 +0800

    Update storyList.vue

--- graduationProgram/pages/story/storyList.vue
+++ graduationProgram/pages/story/storyList.vue
@@ -16,61 +16,31 @@
 	export default {
 		data() {
 			return {
-				placeData: 'A1教学楼',
-				storyList: [{
-						sid: 1,
-						usrname: '我是昵称1',
-						time: '2021-05-01',
-						tags: [{
-								value: 'haoye0'
-							},
-							{
-								value: 'haoye1'
-							},
-
-						],
-						storyContent: '我是内容1 这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。',
-						comment_num: 5,
-						mark_flag: false, //获取用户数据
-						like_flag: false, //获取用户数据
-						like_num: 50,
-						mark_icon: '../../static/story/icon/mark.png',
-						mark_after_icon: '../../static/story/icon/mark_after.png',
-						like_icon: '../../static/story/icon/like.png',
-						like_after_icon: '../../static/story/icon/like_after.png',
-						replyContent: '',
-						commentList: [],
-					},
-					{
-						sid: 1,
-						usrname: '我是昵称2',
-						time: '2021-05-01',
-						tags: [{
-								value: 'haoye0'
-							},
-							{
-								value: 'haoye1'
-							},
-
-						],
-						storyContent: '我是内容2 这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。这里是文章测试标题，含标点一共二十个字。',
-						comment_num: 5,
-						mark_flag: false, //获取用户数据
-						like_flag: false, //获取用户数据
-						like_num: 50,
-						mark_icon: '../../static/story/icon/mark.png',
-						mark_after_icon: '../../static/story/icon/mark_after.png',
-						like_icon: '../../static/story/icon/like.png',
-						like_after_icon: '../../static/story/icon/like_after.png',
-						replyContent: '',
-						commentList: [],
-					},
-				]
-
+				placeData: ['大学城校区','A1教学楼'],
+				storyList: [],
 			}
 		},
 		methods: {},
 		onLoad() {
+			api.getStory({
+				location_id: this.placeData,
+				page_id: 1,
+			}).then(res => {
+				console.log("storyList", res);
+				const temp = JSON.parse(JSON.stringify(res.data.storyList));
+				for(var i=0;i<temp.length;i++){
+					this.storyList.push({
+						sid: temp[i].sid,
+						usrname: '我是昵称1',//未获取
+						time: temp[i].pubDate,
+						tags: temp[i].tagsList,//"tagsList": ["青春回忆"],
+						storyContent: temp[i].detail,
+						comment_num: 5,//未获取
+						mark_flag: temp[i].collectFlag,
+						like_flag: temp[i].goodFlag,
+						like_num: temp[i].goodCount,
+					})
+				}
 			//获取发布人昵称、头像、发布日期、标签、点赞、评论、收藏
 			for (var i = 0; i < this.storyList.length; i++) {
 				api.getComments({
